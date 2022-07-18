c0131b9a7c4eb1833decd609e8271dcada847396
Drew Guild <drewguild@MacBook-Pro-5.hsd1.vt.comcast.net>
2022-07-18 16:06:34 -0400

    Setup basic proxying

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+require('dotenv').config()
+
 const app = express()
 const port = 3001
+const API_SERVICE_URL = "https://api.airtable.com/v0/appUFi3IVRgOv95nN/Points";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(morgan('dev'));
+
+app.use('/data', createProxyMiddleware({
+  target: API_SERVICE_URL,
+  changeOrigin: true,
+  pathRewrite: {
+    [`^/data`]: '',
+  },
+  headers: {
+    'Authorization': `Bearer ${process.env.AIRTABLE_API_KEY}`
+  }
+}))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
